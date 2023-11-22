da0d31b24e8147d8e0f65850e1cb7252e7757f49
Paul Merkamp <pmerkamp@butler.edu>
2023-11-22 10:01:49 -0500

    initial commit

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,14 +1,36 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using LC_API.BundleAPI;
+using UnityEngine;
 
-namespace LethalCompanyTemplate
+namespace LCFartLizards
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Awake()
+        private Harmony harmony;
+
+        public static AudioClip audioClip;
+
+        public void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            harmony = new Harmony("LCFartLizards");
+            harmony.PatchAll();
+        }
+
+        public static bool audioClipPlayed = false;
+
+        [HarmonyPatch(typeof(PufferAI), "Start")]
+        public class PufferAIPatch
+        {
+            public static void Postfix(PufferAI __instance)
+            {
+                AudioClip audioClip = BundleLoader.GetLoadedAsset<AudioClip>("assets/LizardSound.mp3");
+                __instance.puff = audioClip;
+            }
         }
     }
 }
---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LCFartLizards
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LCFartLizards";
+        public const string PLUGIN_NAME = "LCFartLizards";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
